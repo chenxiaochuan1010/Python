8656e8d930e34903dce25c3208adaeade2572be2
Chen Xiaochuan <chenxiaochuan1010@googlemail.com>
2020-02-16 11:57:41 +0800

    Wolrd Map

--- data_visualization/highs_lows.py
+++ data_visualization/highs_lows.py
@@ -1,29 +1,37 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-# Get high temperatures from file.
-filename = 'sitka_weather_07-2018_simple.csv'
+# Get dates, high and low temperatures from file.
 # filename = 'sitka_weather_2018.csv'
+filename = 'death_valley_2018_full.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
-        high = int(row[5])
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
+
+        high = row[8]
         highs.append(high)
 
-    print(highs)
+        low = row[9]
+        lows.append(low)
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
